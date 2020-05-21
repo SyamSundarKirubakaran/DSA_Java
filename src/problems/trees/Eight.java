2afec3d67ee5799666c6ee855d77c0dc4770f039
Syam Sundar K <k.syamsundar21@gmail.com>
2020-05-21 20:38:57 +0530

    Tree Problems added
    Signed-off-by: Syam Sundar K <k.syamsundar21@gmail.com>

---
+++ src/problems/trees/Eight.java
@@ -0,0 +1,34 @@
+package problems.trees;
+
+/**
+ * @author SYAM K
+ * @problem : Diameter of the Binary Tree
+ */
+public class Eight {
+
+    public static void main(String[] args) {
+        Node head = new Node().constructBalancedTree();
+        System.out.println(diameter(head, new Height(0)));
+    }
+
+    private static int diameter(Node node, Height height) {
+        Height leftHeight = new Height(0), rightHeight = new Height(0);
+        if (node == null) {
+            height.height = 0;
+            return 0;
+        }
+        int leftDiameter = diameter(node.left, leftHeight);
+        int rightDiameter = diameter(node.right, rightHeight);
+        height.height = Math.max(leftHeight.height, rightHeight.height) + 1;
+        return Math.max((leftHeight.height + rightHeight.height + 1), Math.max(leftDiameter, rightDiameter));
+    }
+
+    static class Height {
+        int height;
+
+        Height(int value) {
+            height = value;
+        }
+    }
+
+}
---
+++ src/problems/trees/Ten.java
@@ -0,0 +1,26 @@
+package problems.trees;
+
+/**
+ * @author SYAM K
+ * @problem : Print nodes at k distance form root
+ */
+public class Ten {
+
+    public static void main(String[] args) {
+        Node head = new Node().constructBalancedTree();
+        printKFromRoot(head, 2, 0);
+    }
+
+    private static void printKFromRoot(Node node, int k, int distance) {
+        if (node != null) {
+            if (distance == k) {
+                System.out.print(node.value + " ");
+                return;
+            }
+            printKFromRoot(node.left, k, distance + 1);
+            printKFromRoot(node.right, k, distance + 1);
+        }
+    }
+
+
+}
