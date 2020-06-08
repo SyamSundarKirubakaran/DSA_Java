8eb3632aea706ece53695ef4c1f92752bfe77b06
Syam Sundar K <k.syamsundar21@gmail.com>
2020-06-08 12:43:06 +0530

    DP problems updated
    Signed-off-by: Syam Sundar K <k.syamsundar21@gmail.com>

---
+++ src/problems/dp/Sixteen.java
@@ -0,0 +1,53 @@
+package problems.dp;
+
+import java.util.Arrays;
+
+/**
+ * @author SYAM K
+ * @problem : Longest non overlapping repeating sub string
+ */
+public class Sixteen {
+
+    public static void main(String[] args) {
+        Long start = System.nanoTime();
+
+        String str = "runjogrun";
+
+        longestRepeatingSubString(str.toCharArray(), str.length());
+
+        Long end = System.nanoTime();
+        System.out.println("Runtime:" + (end - start) / 1.0e9 + " seconds");
+    }
+
+    private static void longestRepeatingSubString(char[] str, int length) {
+        int[][] lrs = new int[length + 1][length + 1];
+        StringBuilder res = new StringBuilder();
+        int resLength = 0;
+
+        int index = 0;
+        for (int i = 1; i <= length; i++) {
+            for (int j = i + 1; j <= length; j++) {
+                if (str[i - 1] == str[j - 1] && (j - i) > lrs[i - 1][j - 1]) { // after && -> overlap condition
+                    lrs[i][j] = lrs[i - 1][j - 1] + 1;
+                    if (lrs[i][j] > resLength) {
+                        resLength = lrs[i][j];
+                        index = Math.max(index, i);
+                    }
+                } else {
+                    lrs[i][j] = 0;
+                }
+            }
+        }
+
+        if (resLength > 0) {
+            for (int i = index - resLength + 1; i <= index; i++) {
+                res.append(str[i - 1]);
+            }
+        }
+        for (int[] lr : lrs) {
+            System.out.println(Arrays.toString(lr));
+        }
+        System.out.println(res);
+    }
+
+}
