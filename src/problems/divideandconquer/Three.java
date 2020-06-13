fb7a6d8d002a37ee07d00e836408397d53e9121e
Syam Sundar K <k.syamsundar21@gmail.com>
2020-06-13 07:28:19 +0530

    Divide and Conquer Init
    Signed-off-by: Syam Sundar K <k.syamsundar21@gmail.com>

--- src/problems/divideandconquer/Three.java
+++ src/problems/divideandconquer/Three.java
@@ -9,7 +9,7 @@ public class Three {
     public static void main(String[] args) {
         Long start = System.nanoTime();
 
-        System.out.println(power(2, 5));
+        System.out.println(power(3, 2));
 
         Long end = System.nanoTime();
         System.out.println("Runtime:" + (end - start) / 1.0e9 + " seconds");
