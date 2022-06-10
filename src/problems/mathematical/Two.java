0367f885f6d1628d7d94a5711cd8e43f70cbe226
Syam Sundar K <k.syamsundar21@gmail.com>
2022-06-10 22:18:00 +0530

    review changes

--- src/problems/mathematical/Two.java
+++ src/problems/mathematical/Two.java
@@ -21,10 +21,10 @@ public class Two {
         for (int i = 0; i < n; i++) {
             prime[i] = true;
         }
-        for (int p = 2; p * p <= n; p++) {
-            if (prime[p] == true) {
-                for (int i = p * p; i <= n; i += p) {
-                    prime[i] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (prime[i] == true) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = false;
                 }
             }
         }
